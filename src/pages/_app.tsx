539f9f226440aa6a77d07ffcd2b98c03c0bd7641
Sinan <sinantalebi@protonmail.com>
2021-05-14 21:28:42 +0200

    removes amplify config from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,21 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 import React from "react";
-import { Amplify } from "aws-amplify";
-import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+// import { Amplify } from "aws-amplify";
+// import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
 import "../styles/globals.css";
 import Navbar from "../components/Layout/Navbar";
-import awsExports from "../aws-exports";
+// import awsExports from "../aws-exports";
 
-Amplify.configure({ ...awsExports, ssr: true });
+// Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AmplifyAuthenticator>
-      <div className="min-h-screen bg-green-100">
-        <Navbar />
-        <main className="-mt-32">
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </AmplifyAuthenticator>
+    <div className="min-h-screen bg-green-100">
+      <Navbar />
+      <main className="-mt-32">
+        <Component {...pageProps} />
+      </main>
+    </div>
   );
 }
 
